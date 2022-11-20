5750190a760abfb08ae4ee330426db9bffdfad21
Sergey Orlovskiy <sergey.orlovskiy667@gmail.com>
2022-11-20 14:03:35 +0300

    refactoring homework

--- src/test/java/com/demoqa/PracticeFormTests.java
+++ src/test/java/com/demoqa/PracticeFormTests.java
@@ -1,78 +1,68 @@
 package com.demoqa;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
-    private String path = "src/test/resources/1.jpg";
     private final String FIRST_NAME = "Egor";
     private final String LAST_NAME = "Semenov";
     private final String EMAIL = "egorsemenov666@gmail.com";
     private final String GENDER = "Male";
     private final String MOBILE = "1234567891";
-    private final String BIRTHDAY = "01 January,1989";
+    private final String BIRTHDAY = "30 January,1989";
     private final String SUBJECTS = "Maths";
     private final String HOBBIES = "Music";
     private final String PICTURES = "1.jpg";
     private final String ADDRESS = "asdasd";
-    private final String STATE_AND_CITY = "Uttar Pradesh Agra";
+    private final String STATE_AND_CITY = "NCR Delhi";
 
 
     @BeforeAll
     static void setUp() {
         System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver.exe");
-        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1932x1160";
         Configuration.browserPosition = "-6x0";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        $("#firstName").val("Egor");
-        $("#lastName").val("Semenov");
-        $("#userEmail").val("egorsemenov666@gmail.com");
-        $$(".custom-radio .custom-control-label").find(text("Male")).click();
-        $("#userNumber").val("1234567891");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").val(FIRST_NAME);
+        $("#lastName").val(LAST_NAME);
+        $("#userEmail").val(EMAIL);
+        // переделать под клик по айди элемента
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").val(MOBILE);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").click();
         $(".react-datepicker__year-select").selectOption("1989");
-        $(".react-datepicker__month-select").click();
         $(".react-datepicker__month-select").selectOption("January");
-        $(byXpath("//div[@class='react-datepicker__day react-datepicker__day--001 react-datepicker__day--weekend']")).click();
-        $("#subjectsInput").val("Maths").pressEnter();
-        $$(".custom-checkbox .custom-control-label").find(text("Music")).click();
-        $("#uploadPicture").uploadFile(new File(path));
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").val(SUBJECTS).pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("1.jpg");
         $("#currentAddress").click();
-        $("#currentAddress").val("asdasd");
+        $("#currentAddress").val(ADDRESS);
         $("#state").click();
-        $("#react-select-3-option-1").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
-        $("#react-select-4-option-0").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
 
-        $(".table-responsive").shouldBe(visible);
-        $(".table-responsive").shouldHave(text(FIRST_NAME));
-        $(".table-responsive").shouldHave(text(LAST_NAME));
-        $(".table-responsive").shouldHave(text(EMAIL));
-        $(".table-responsive").shouldHave(text(GENDER));
-        $(".table-responsive").shouldHave(text(MOBILE));
-        $(".table-responsive").shouldHave(text(BIRTHDAY));
-        $(".table-responsive").shouldHave(text(SUBJECTS));
-        $(".table-responsive").shouldHave(text(HOBBIES));
-        $(".table-responsive").shouldHave(text(PICTURES));
-        $(".table-responsive").shouldHave(text(ADDRESS));
-        $(".table-responsive").shouldHave(text(STATE_AND_CITY));
-        $("#closeLargeModal").shouldHave(enabled).click();
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(FIRST_NAME), text(LAST_NAME), text(EMAIL),
+                text(GENDER), text(MOBILE), text(BIRTHDAY), text(SUBJECTS), text(HOBBIES),
+                text(PICTURES), text(ADDRESS), text(STATE_AND_CITY));
+        $("#closeLargeModal").shouldHave(appear).click();
     }
 }
